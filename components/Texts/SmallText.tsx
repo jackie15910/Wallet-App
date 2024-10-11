81655b280e625d53b33d7fa7b21cadf5437a05d5
Jackie <jackie15910@gmail.com>
2024-10-10 22:53:36 -0400

    Add smalltext

---
+++ components/Texts/SmallText.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from "react";
+import styled from "styled-components/native";
+
+import { colors } from "../colors";
+
+const StyledText = styled.Text`
+    font-size: 13px;
+    color: ${colors.gray};
+    text-align: left;
+`;
+
+const SmallText: FunctionComponent = () => {
+    return <></>;
+};
+
+export default SmallText;
